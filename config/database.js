4588cc372cbf972e35f048f1532d1e473e3c91b6
luzimar <lzm.spfc@hotmail.com>
2019-11-08 21:11:00 -0200

    first commit

--- config/database.js
+++ config/database.js
@@ -1,36 +1,11 @@
 "use strict";
 
 const Env = use("Env");
-const Url = require('url-parse')
-const DATABASE_URL = new Url(Env.get('DATABASE_URL'))
-
-const Helpers = use("Helpers");
+const Url = require("url-parse");
+const DATABASE_URL = new Url(Env.get("DATABASE_URL"));
 
 module.exports = {
-  connection: Env.get("DB_CONNECTION", "sqlite"),
-
-  sqlite: {
-    client: "sqlite3",
-    connection: {
-      filename: Helpers.databasePath(
-        `${Env.get("DB_DATABASE", "development")}.sqlite`
-      )
-    },
-    useNullAsDefault: true,
-    debug: Env.get("DB_DEBUG", false)
-  },
-
-  mysql: {
-    client: "mysql",
-    connection: {
-      host: Env.get("DATABASE_URL", "localhost"),
-      port: Env.get("DB_PORT", ""),
-      user: Env.get("DB_USER", "root"),
-      password: Env.get("DB_PASSWORD", ""),
-      database: Env.get("DB_DATABASE", "adonis")
-    },
-    debug: Env.get("DB_DEBUG", false)
-  },
+  connection: Env.get("DB_CONNECTION", "pg"),
 
   pg: {
     client: "pg",
